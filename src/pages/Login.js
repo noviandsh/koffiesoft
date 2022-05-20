0559fa0ef447cfb5ff155e84e4909a9f81d7be2e
noviandsh <noviandsh@gmail.com>
2022-05-21 00:44:58 +0700

    add login page

---
+++ src/pages/Login.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+export default function Login() {
+    return (
+        <div>
+            <h1 className='mb-4'>Login</h1>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+                </div>
+                <button type="submit" className="btn btn-primary me-3">Login</button> <Link to="/register">Register</Link>
+            </form>
+        </div>
+    )
+}
