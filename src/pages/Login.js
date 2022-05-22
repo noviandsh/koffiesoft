b71dabe3a9ca278551f1e81760224c2849622720
noviandsh <noviandsh@gmail.com>
2022-05-22 07:49:50 +0700

    add login function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,70 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import API_ENDPOINT from '../api/api-endpoint'
 
 export default function Login() {
+    const navigate = useNavigate()
     const [form, setForm] = useState({
         username: '',
         password: ''
     })
-    const handleSubmit = (e) => {
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            navigate('/dashboard')
+        }
+    }, [])
+
+    const encodeForm = (data) => {
+        let formBody = []
+        for (let property in data) {
+            let encodedKey = encodeURIComponent(property)
+            let encodedValue = encodeURIComponent(data[property])
+            formBody.push(encodedKey + "=" + encodedValue)
+        }
+        return formBody.join('&')
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(form);
+        await fetch(API_ENDPOINT.LOGIN, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: encodeForm(form)
+        })
+            .then((res) => res.json())
+            .then((resJson) => {
+                if (resJson.status.kode === "success") {
+                    localStorage.setItem('token', resJson.access_token)
+                    localStorage.setItem('status', JSON.stringify(resJson.data.account_status))
+                    navigate('/dashboard')
+                } else {
+                    Swal.fire(
+                        'Login failed',
+                        resJson.status.keterangan,
+                        'error',
+                    );
+                }
+
+            })
+            .catch(() => {
+                if (!window.navigator.onLine) {
+                    Swal.fire(
+                        'Login failed',
+                        'Make sure your internet is connected!',
+                        'error',
+                    );
+                } else {
+                    Swal.fire(
+                        'Login failed',
+                        '',
+                        'error',
+                    );
+                }
+            })
     }
     return (
         <div>
@@ -26,7 +82,7 @@ export default function Login() {
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                     <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
                 </div>
-                <button type="submit" className="btn btn-primary me-3">Login</button> <Link to="/register">Register</Link>
+                <button type="submit" className="btn btn-primary me-3 px-4">Login</button> Don't have an account? <Link to="/register">Register</Link>
             </form>
         </div>
     )
