49f9053763e5dce96c6ff9f9e639916929e6c9c4
noviandsh <noviandsh@gmail.com>
2022-05-22 07:49:21 +0700

    remove detail endpoint

--- src/api/api-endpoint.js
+++ src/api/api-endpoint.js
@@ -2,10 +2,9 @@ const BASE_URL = "http://202.157.184.201:8000/"
 
 const API_ENDPOINT = {
     LOGIN: `${BASE_URL}login`,
-    DETAIL: (id) => `${BASE_URL}users/${id}`,
     REGISTER: `${BASE_URL}users`,
-    VERIFICATION: `${BASE_URL}verifikasi`,
-    OTP_RESEND: `${BASE_URL}otp`,
+    VERIFICATION: `${BASE_URL}users/verifikasi`,
+    OTP_RESEND: `${BASE_URL}users/otp`,
 }
 
 export default API_ENDPOINT
