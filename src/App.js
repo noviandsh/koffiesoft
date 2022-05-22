53a97743a5888e87a6e65dbd51f71ba716a0afb6
noviandsh <noviandsh@gmail.com>
2022-05-22 07:49:03 +0700

    add styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [dashboardVerified, setDashboardVerified] = useState(false)
+  const [mainStyle, setMainStyle] = useState('')
+
+  useEffect(() => {
+    setMainStyle(dashboardVerified ? 'w-75 h-75 bg-white' : 'text-white w-50')
+  }, [dashboardVerified])
+
   return (
     <Router>
-      <main className='text-white w-50 py-5 px-4 rounded'>
+      <main className={'py-5 px-4 rounded ' + mainStyle}>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard setDashboardVerified={(state) => setDashboardVerified(state)} />} />
         </Routes>
       </main>
     </Router>
